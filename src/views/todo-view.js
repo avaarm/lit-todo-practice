674dd4c5a4a613286aa11f6ad6c931707cbfd4cf
Armenuhi Avanesyan <aavanesy@SR-PD-C02HF0D3Q05Q.local>
2022-05-19 10:47:34 -0700

    adding first views

--- src/views/todo-view.js
+++ src/views/todo-view.js
@@ -1,6 +1,30 @@
 import { LitElement, html} from 'lit-element'; (1)
 
+const VisibilityFilters = { 
+    SHOW_ALL: 'ALL',
+    SHOW_ACTIVE: 'Active',
+    SHOW_COMPLETED: 'Completed'
+}
+
+
 class TodoView extends LitElement {
+
+    static get properties() {
+        return{
+            todos: {type:Array},
+            filter: {type: String},
+            task: {type: String}
+        };
+    }
+
+    constructor() {
+        super();
+        this.todos = [];
+        this.filter = VisibilityFilters.SHOW_ALL;
+        this.task = '';
+    }
+
+
     render() { (2)
        return html 
         ` 
@@ -15,5 +39,6 @@ customElements.define('todo-view', TodoView); (4)
 // (2) Create a class for the component, extending LitElement
 // (3) Define a template in the render() function using the html template function
 // (4) Associate the component implementation with an HTML tag using the CustomElements registry
-
+// (5) Define an object with the different filter options
+// (6) Define a static getter for properties that returns an object that defines the peroperty names and types 
 // note - tag names need to have a dash (-) to avoid naming collisions with native HTML elements
